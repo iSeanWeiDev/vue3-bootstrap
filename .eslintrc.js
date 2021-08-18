b74e4ecf5889b4bdb6dc731cbb217b2e5b10ecf7
iStevePeters <dev.steve452@gmail.com>
2021-08-17 22:02:41 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    jest: true,
+    es2020: true,
+    node: true,
+    browser: true
+  },
+  extends: ['prettier', 'airbnb-base', 'plugin:vue/vue3-essential'],
+  // required to lint *.vue files
+  plugins: ['prettier', 'vue'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-console': ['off'],
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never'
+      }
+    ],
+    indent: 'off',
+    'object-curly-newline': 'off',
+    'import/prefer-default-export': 'off',
+    'import/named': 'off',
+    'operator-linebreak': 'off',
+    'func-names': 'off',
+    'object-shorthand': 'off',
+    'linebreak-style': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack/webpack.base.conf.js'
+      }
+    }
+  }
+};
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@router': path.resolve(__dirname, 'src/router'),
+        '@store': path.resolve(__dirname, 'src/store'),
+        '@views': path.resolve(__dirname, 'src/views'),
+      }
+    }
+  }
+}
