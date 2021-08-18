f588b7102b4f8dc6b71ef3140fcde1fa00f49422
iStevePeters <dev.steve452@gmail.com>
2021-08-17 22:29:19 -0700

    added bootstrap

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,14 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button
+    class="btn btn-primary"
+    data-bs-target="#collapseTarget"
+    data-bs-toggle="collapse"
+  >
+    Bootstrap collapse
+  </button>
+  <div class="collapse py-2" id="collapseTarget">
+    This is the toggle-able content!
+  </div>
 </template>
